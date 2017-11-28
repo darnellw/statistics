86fea5266ddd19b2b92668ee6c50864a3bdb5fc3
darnellw <33166092+darnellw@users.noreply.github.com>
2017-11-27 22:25:51 -0500

    Expanded basic_stats.sh and updated test data.

--- basic_stats.sh
+++ basic_stats.sh
@@ -4,59 +4,94 @@
 # basic statictics about that data.
 
 # Quit if file not found
-if [ ! -f $1 ]; then
+if [ ! -f "$1" ]; then
   echo "Error: file $1 not found."
   exit 1
+fi
+
+# Preserve existing IFS
+OLD_IFS=$IFS
+
+# Check if IFS is supplied
+if [ $# -eq 2 ]; then
+  case $2 in
+    space)
+      IFS=' '
+      ;;
+    comma)
+      IFS=','
+      ;;
+    semicolon)
+      IFS=';'
+      ;;
+    *)
+      echo "Warning: IFS specifier unrecognized. Using default setting."
+  esac
 fi
 
 # Table formatting
 table="%-20s %s \n"
 
-# Variables
-sum=0               # Sum
-n=0                 # Count
-min=$(head -n 1 $1) # Minimum
-max=$(head -n 1 $1) # Maximum
-mean=0              # Mean
-sx=0                # Standard deviation
-v=0                 # Variance
+# Initially set min and max to the first value in the data set
+min=$(head -n 1 "$1")
+max=$(head -n 1 "$1")
+
+# Read data file into array
+raw=()
+while read data_value; do
+  raw+=("$data_value")
+done < "$1"
+
+# Sort array
+data=($(sort <<< "${raw[@]}"))
+
+# Ensure IFS is set to default
+IFS=OLD_IFS
 
 # Find sum and count
-while read num; do
+for num in "${data[@]}"; do
   sum=$((sum+num))
-  if [ "$num" -lt "$min" ]; then
-    min=$num
-  elif [ "$num" -gt "$max" ]; then
-    max=$num
+  if ((num < min)); then
+    min="$num"
+  elif ((num > max)); then
+    max="$num"
   fi
   ((n++))
-done < $1
+done
 
 # Find mean
 mean=$(bc <<< "scale=8;$sum/$n")
 
 # Find the variance and standard deviation
 sum_sq=0
-while read num; do
+for num in "${data[@]}"; do
   diff=$(bc <<< "scale=8;$num-$mean")
   sq_diff=$(bc <<< "scale=8;$diff^2")
   sum_sq=$(bc <<< "scale=8;$sum_sq+$sq_diff")
-done < $1
+done
 v=$(bc <<< "scale=8;$sum_sq/($n-1)")
 sx=$(bc <<< "scale=4;sqrt($v)")
 
+# Find the median
+if ((n % 2 == 0)); then
+  med=$((data[(n/2)-1]+data[(n/2)]))
+  med=$(bc <<< "scale=8;$med/2")
+else
+  med=$((data[n/2]))
+fi
+
 # Print table header
 header="Descriptive Statistics of \"$1\""
-echo $header
+echo "$header"
 
 # Print table underline
 size=${#header}; i=0
-while [ "$i" -lt "$size" ]; do
+while ((i < size)); do
   printf "="
   ((i++))
 done; echo
 
-# Print statistics
+# Print descriptive statistics
 printf "$table" \
 "Sum" "$sum" \
 "Count" "$n" \
@@ -64,4 +99,5 @@ printf "$table" \
 "Max" "$max" \
 "Mean" "$mean" \
 "Variance" "$v" \
-"Standard deviation" "$sx"
+"Standard deviation" "$sx" \
+"Median" "$med"
